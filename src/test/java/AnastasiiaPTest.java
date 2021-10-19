bfd9653d24a3f9b00971650b9b121ee545ba9501
Anastasiia Potapenko <82536794+AnastasiiaPV@users.noreply.github.com>
2021-10-19 13:18:45 -0400

    AP_RefactoringOldAndNewOneTest (#94)
    * Refactoring and one new Test
    
    * Refactoring
    
    Co-authored-by: Anastasiia Potapenko <oberonapa@gmail.como>

--- src/test/java/AnastasiiaPTest.java
+++ src/test/java/AnastasiiaPTest.java
@@ -3,12 +3,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AnastasiiaPTest {
@@ -35,10 +37,13 @@ public class AnastasiiaPTest {
     @Test
     public void testCurrentAddressTextField() {
         String expectedResult = "Roses Delivery";
-
         driver.get("https://www.1800flowers.com/");
-        driver.findElement(By.id("SearchBox_desktop")).sendKeys("Roses \n");
-        driver.findElement(By.id("btn-search"));
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("SearchBox_desktop")));
+
+        driver.findElement(By.id("SearchBox_desktop")).sendKeys("Roses");
+        driver.findElement(By.id("btn-search")).click();
 
         WebElement actualResult = driver.findElement(By.xpath("//div[h1]"));
 
@@ -59,4 +64,27 @@ public class AnastasiiaPTest {
     }
 }
 
+    @Test
+    public void testAvailabilityMenuButtons() {
+        String[] expectedArray = new String[]{
+                "ABOUT OUR ICE CREAM",
+                "ORDER ONLINE",
+                "SIGNATURE CREATIONS",
+                "CREATE YOUR OWN CREATIONS",
+                "SUNDAES",
+                "MIX & GO",
+                "PRE-PACKED",
+                "FAMILY PACKS",
+                "ICE CREAM FAQ"};
+
+        driver.get("https://www.coldstonecreamery.com/index.php");
+        driver.findElement(By.xpath("//div[contains(@class,'close-button')]")).click();
+        driver.findElement(By.xpath("//a[contains(@aria-controls,'icecream')]")).click();
+
+        List<WebElement> menuList = driver.findElements(By.xpath("//ul[contains(@id,'icecream')]//li"));
+        for (int i = 0; i < menuList.size(); i++) {
+            Assert.assertEquals(menuList.get(i).getText(),expectedArray[i]);
+        }
+    }
+}
 
