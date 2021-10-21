cbc668422eab93e5b7a08215ce5612dedd5a402c
tim-yoshi <i5138011Ig>
2021-10-20 21:45:02 -0400

    Refactor. Renamed dills to driver.

--- src/test/java/KosherDillsTest.java
+++ src/test/java/KosherDillsTest.java
@@ -1,7 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -12,32 +11,32 @@ import java.util.concurrent.TimeUnit;
 
 public class KosherDillsTest {
 
-    private WebDriver dills;
+    private WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
 
-        dills = new ChromeDriver();
+        driver = new ChromeDriver();
 
-        dills.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        dills.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
 
     @AfterMethod
 
     public void shutDown() throws InterruptedException {
-        dills.quit();
+        driver.quit();
     }
 
     @Test
     public void testGoogleSearch() {
 
-        dills.get("https://www.google.com");
-        dills.findElement(By.xpath("//input[@title='Search']"))
+        driver.get("https://www.google.com");
+        driver.findElement(By.xpath("//input[@title='Search']"))
                 .sendKeys("RuAmerica" + "\n");
 
-        Assert.assertEquals(dills
+        Assert.assertEquals(driver
                 .findElement(By.xpath("//cite[text()='https://ruamerica.com']"))
                 .getText(), "https://ruamerica.com");
 
