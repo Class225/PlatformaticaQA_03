a65c1eb320f1d28799dd3171280b827075393402
Stanislav <kaplanst@mail.ru>
2021-10-23 18:20:09 -0400

    testDoubleDropDownSelect changing

--- src/test/java/KosherDillsTest.java
+++ src/test/java/KosherDillsTest.java
@@ -10,12 +10,12 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class KosherDillsTest {
 
     private WebDriver driver;
+    private final By ERROR = By.xpath("//div[@class='alert alert-info']/b");
 
     @BeforeMethod
     public void setUp() {
@@ -67,23 +67,22 @@ public class KosherDillsTest {
         driver.get("https://bidfax.info/");
 
         driver.findElement(By.xpath("(//span[@class='drop-down'])[1]")).click();
-        driver.findElement(By.xpath("//a[normalize-space()='Kia']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Aston martin']")).click();
         driver.findElement(By.xpath("(//span[@class='drop-down'])[2]")).click();
-        driver.findElement(By.xpath("//a[normalize-space()='Seltos']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Db9']")).click();
 
-        int pageDepth = 5;
-
-        for (int i = 0; i < pageDepth; i++) {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
             List<WebElement> cars = driver.findElements(By.xpath("//div[@class='caption']/a/h2"));
             Assert.assertTrue(carsCheck(cars));
             driver.findElement(By.xpath("//a[normalize-space()='»']")).click();
+                if (driver.findElements(ERROR).size() > 0) break;
         }
     }
 
     public boolean carsCheck (List<WebElement> var1){
         int trueCount = 0;
         for (int i = 0; i < var1.size(); i++) {
-            if (var1.get(i).getText().toLowerCase().contains("seltos"))
+            if (var1.get(i).getText().toLowerCase().contains("db9"))
                 trueCount++;
         }
         if (trueCount == var1.size()){return true;}
