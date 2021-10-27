575f35faef870e25efff060a8df249f82d8da38c
sale <Stock2018>
2021-10-27 11:28:48 -0700

    add getDriver()

--- src/test/java/Group_eat_and_drink_JavaTest.java
+++ src/test/java/Group_eat_and_drink_JavaTest.java
@@ -471,21 +471,21 @@ public class Group_eat_and_drink_JavaTest extends BaseTest {
 
     @Test
     private void SergeyBrigEddToCartTest() {
-        driver.get("https://www.webstaurantstore.com");
+        getDriver().get("https://www.webstaurantstore.com");
 
-        driver.findElement(By.xpath("//input[@id = 'searchval']")).sendKeys("220SPRDWNEN3"+ "\n");
+        getDriver().findElement(By.xpath("//input[@id = 'searchval']")).sendKeys("220SPRDWNEN3"+ "\n");
 
-        List<WebElement> sizeVariations = driver.findElements(By.xpath("//ul[@class = 'nav nav-pills nav-stacked']/li"));
+        List<WebElement> sizeVariations = getDriver().findElements(By.xpath("//ul[@class = 'nav nav-pills nav-stacked']/li"));
         for (int i = 0; i < sizeVariations.size(); i++) {
             if(sizeVariations.get(i).getText().contains("5 1/2\"")) {
                 sizeVariations.get(i).click();
                 break;
             }
         }
-        driver.findElement(By.xpath("//input[@id ='qty']")).sendKeys("2");
-        driver.findElement(By.xpath("//input[@id ='buyButton']")).click();
-        driver.findElement(By.xpath("//a[@data-testid='cart-nav-link']")).click();
-        WebElement orderItem = driver.findElement(By.xpath("//span[@id= 'cartItemCountSpan']"));
+        getDriver().findElement(By.xpath("//input[@id ='qty']")).sendKeys("2");
+        getDriver().findElement(By.xpath("//input[@id ='buyButton']")).click();
+        getDriver().findElement(By.xpath("//a[@data-testid='cart-nav-link']")).click();
+        WebElement orderItem = getDriver().findElement(By.xpath("//span[@id= 'cartItemCountSpan']"));
 
         Assert.assertEquals(orderItem.getText(), "21");
 
