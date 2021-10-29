d3130b4fab23c547279a53d99cccb5181faa4abb
Vladimir <umistester@gmail.com>
2021-10-29 21:40:05 +0300

    First test

--- src/test/java/VladimirMTest.java
+++ src/test/java/VladimirMTest.java
@@ -7,38 +7,33 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class VladimirMTest {
+
     @Test
-    public void testSelenium() throws InterruptedException {
+    public void testSelenium() {
 
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        driver.get("https://av.by/login");
+        driver.get("https://av.by");
 
-        WebElement tab = driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div/div/div/div/div[1]/div/div[1]/button[2]"));
-        tab.click();
+        WebElement bmw = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[2]/div[1]/main[1]/div[1]/div[1]" +
+                "/div[1]/div[2]/div[1]/div[2]/div[1]/ul[3]/li[1]/a[1]/span[1]"));
 
-        WebElement username = driver.findElement(By.id("login"));
-        WebElement password = driver.findElement(By.xpath("//body/div[@id='__next']/div[2]/main[1]/div[1]/div[1]/div[1]" +
-                "/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/div[1]/div[2]/div[2]/input[1]"));
-        WebElement login = driver.findElement(By.xpath("//body/div[@id='__next']/div[2]/main[1]/div[1]/div[1]/div[1]/" +
-                "div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/div[1]/div[4]/button[1]"));
+        bmw.click();
 
-//        Необходима регистрация
-//        username.sendKeys("--------");
-//        password.sendKeys("--------");
+        WebElement show = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[1]/div" +
+                "[4]/form[1]/div[1]/div[3]/div[1]/div[3]/div[2]/div[2]/button[1]"));
+        show.click();
 
-        login.click();
 //        Thread.sleep(3000);
 
-        String expectedUrl = "https://av.by/";
+        String expectedUrl = "https://cars.av.by/bmw";
         String actualUrl = driver.getCurrentUrl();
 
         Assert.assertEquals(actualUrl, expectedUrl);
 
         driver.quit();
     }
-
 }
