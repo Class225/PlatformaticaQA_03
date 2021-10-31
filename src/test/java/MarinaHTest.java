38d7d88cd740c0f7fabc1a10ba9e62fe8f1d3024
marinaH <marinahortobagyi66@gmail.com>
2021-10-31 18:02:24 -0400

    Code refactoring extends Base Test

--- src/test/java/MarinaHTest.java
+++ src/test/java/MarinaHTest.java
@@ -1,3 +1,4 @@
+import base.BaseTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,44 +12,29 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-@Ignore
-public class MarinaHTest {
-    private WebDriver driver;
 
-    @BeforeMethod
-    public void setUp(){
-        WebDriverManager.chromedriver().setup();
+public class MarinaHTest extends BaseTest {
 
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-    }
-
-    @AfterMethod
-    public void setDown(){
-        driver.quit();
-    }
-
-    @Test
+          @Test
     public void testMarinaHort() throws InterruptedException {
         String expectedResult = "https://my-learning.w3schools.com/";
 
-        driver.get("https://www.w3schools.com/");
+        getDriver().get("https://www.w3schools.com/");
 
-        WebElement logInButton = driver.findElement(By.xpath("//div/a[@id='w3loginbtn']"));
+        WebElement logInButton = getDriver().findElement(By.xpath("//div/a[@id='w3loginbtn']"));
         logInButton.click();
 
-        WebElement emailField = driver.findElement(By.xpath("//input[@class='_ZsgaF undefined']"));
+        WebElement emailField = getDriver().findElement(By.xpath("//input[@class='_ZsgaF undefined']"));
         emailField.sendKeys("marinahortobagyi66@gmail.com");
 
-        WebElement password = driver.findElement(By.xpath("//input[@class='_lEWNL']"));
+        WebElement password = getDriver().findElement(By.xpath("//input[@class='_lEWNL']"));
         password.sendKeys("Mandula66!");
 
-        WebElement logInButton1 = driver.findElement(By.xpath("//button[@class='_1VfsI _OD95i _3_H0V']"));
+        WebElement logInButton1 = getDriver().findElement(By.xpath("//button[@class='_1VfsI _OD95i _3_H0V']"));
         logInButton1.click();
 
         Thread.sleep(7000);
-        String actualResult = driver.getCurrentUrl();
+        String actualResult = getDriver().getCurrentUrl();
 
         Assert.assertEquals(expectedResult, actualResult);
     }
