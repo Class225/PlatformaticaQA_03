feb4bf232eb865ea525cf389b694fc419dca3632
Anastasiia Potapenko <oberonapa@gmail.como>
2021-11-02 16:48:33 -0400

    IgnoreTests

--- src/test/java/Group_eat_and_drink_JavaTest.java
+++ src/test/java/Group_eat_and_drink_JavaTest.java
@@ -1,15 +1,10 @@
 import base.BaseTest;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
@@ -172,6 +167,8 @@ public class Group_eat_and_drink_JavaTest extends BaseTest {
         forgotPassword.click();
         Assert.assertEquals(getDriver().getCurrentUrl(), "https://humans.net/recoveryform");
     }
+
+    @Ignore
     @Test
     public void testVerifyListItemsTatianaT() {
         getDriver().get("http://automationpractice.com/");
@@ -489,7 +486,7 @@ public class Group_eat_and_drink_JavaTest extends BaseTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("header-title"))).click();
         Assert.assertEquals(h2Name.getText(), "Flights reservation module");
     }
-
+    @Ignore
     @Test
     private void SergeyBrigEddToCartTest() {
         getDriver().get("https://www.webstaurantstore.com");
