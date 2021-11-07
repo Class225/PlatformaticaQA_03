cc5b5281247d160795a77e86333f75a662828fd8
Andrey2694 <fordcoppola94@gmail.com>
2021-11-07 14:35:09 +0100

    changed Assert xpath

--- src/test/java/ReferenceValuesTest.java
+++ src/test/java/ReferenceValuesTest.java
@@ -4,22 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ReferenceValuesTest extends BaseTest {
-    private static final String LABEL = "Label";
-    private static final String FILTER1 = "FILTER1";
-    private static final String FILTER2 = "FILTER2";
+    private static final String LABEL_VALUE = "Label";
+    private static final String FILTER1_VALUE = "FILTER1";
+    private static final String FILTER2_VALUE = "FILTER2";
 
     @Test
     void testCreateRecord() {
         getDriver().findElement(By.xpath("//div[@id = 'menu-list-parent']//p[contains(text(), 'Reference values' )]")).click();
         getDriver().findElement(By.className("card-icon")).click();
 
-        getDriver().findElement(By.id("label")).sendKeys(LABEL);
-        getDriver().findElement(By.id("filter_1")).sendKeys(FILTER1);
-        getDriver().findElement(By.id("filter_2")).sendKeys(FILTER2);
+        getDriver().findElement(By.id("label")).sendKeys(LABEL_VALUE);
+        getDriver().findElement(By.id("filter_1")).sendKeys(FILTER1_VALUE);
+        getDriver().findElement(By.id("filter_2")).sendKeys(FILTER2_VALUE);
         getDriver().findElement(By.id("pa-entity-form-save-btn")).click();
 
-        Assert.assertEquals(LABEL, getDriver().findElement(By.xpath("//tr[@data-index = '0']//a[contains(text(),'" + LABEL + "')]")).getText());
-        Assert.assertEquals(FILTER1, getDriver().findElement(By.xpath("//tr[@data-index = '0']//a[contains(text(),'" + FILTER1 + "')]")).getText());
-        Assert.assertEquals(FILTER2, getDriver().findElement(By.xpath("//tr[@data-index = '0']//a[contains(text(),'" + FILTER2 + "')]")).getText());
+        Assert.assertEquals(LABEL_VALUE, getDriver().findElement(By.xpath("//tr[@data-index = '0']/td[2]/a")).getText());
+        Assert.assertEquals(FILTER1_VALUE, getDriver().findElement(By.xpath("//tr[@data-index = '0']/td[3]/a")).getText());
+        Assert.assertEquals(FILTER2_VALUE, getDriver().findElement(By.xpath("//tr[@data-index = '0']/td[4]/a")).getText());
     }
 }
