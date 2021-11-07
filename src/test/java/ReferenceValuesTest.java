c895a42db9aa0c1e5808d0d60c92b49ce29585dc
Andrey2694 <fordcoppola94@gmail.com>
2021-11-07 14:57:57 +0100

    add testEditRecord

--- src/test/java/ReferenceValuesTest.java
+++ src/test/java/ReferenceValuesTest.java
@@ -1,5 +1,7 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -21,5 +23,31 @@ public class ReferenceValuesTest extends BaseTest {
         Assert.assertEquals(LABEL_VALUE, getDriver().findElement(By.xpath("//tr[@data-index = '0']/td[2]/a")).getText());
         Assert.assertEquals(FILTER1_VALUE, getDriver().findElement(By.xpath("//tr[@data-index = '0']/td[3]/a")).getText());
         Assert.assertEquals(FILTER2_VALUE, getDriver().findElement(By.xpath("//tr[@data-index = '0']/td[4]/a")).getText());
+    }
+
+    @Test
+    void testEditRecord() {
+        testCreateRecord();
+
+        getDriver().findElement(By.xpath("//tr[@data-index = '0']/td[last()]//button")).click();
+        getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//ul[@role = 'menu']//a[contains(text(), 'edit')]"))).click();
+
+        WebElement label = getDriver().findElement(By.id("label"));
+        label.clear();
+        label.sendKeys(LABEL_VALUE + " 1");
+
+        WebElement filter1 = getDriver().findElement(By.id("filter_1"));
+        filter1.clear();
+        filter1.sendKeys(FILTER1_VALUE + " 1");
+
+        WebElement filter2 = getDriver().findElement(By.id("filter_2"));
+        filter2.clear();
+        filter2.sendKeys(FILTER2_VALUE + " 1");
+
+        getDriver().findElement(By.id("pa-entity-form-save-btn")).click();
+
+        Assert.assertEquals(LABEL_VALUE + " 1", getDriver().findElement(By.xpath("//tr[@data-index = '0']/td[2]/a")).getText());
+        Assert.assertEquals(FILTER1_VALUE + " 1", getDriver().findElement(By.xpath("//tr[@data-index = '0']/td[3]/a")).getText());
+        Assert.assertEquals(FILTER2_VALUE + " 1", getDriver().findElement(By.xpath("//tr[@data-index = '0']/td[4]/a")).getText());
     }
 }
