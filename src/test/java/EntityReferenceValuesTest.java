e9cd45d767956db69568b98d991b74f5d85e8aad
Andrey2694 <fordcoppola94@gmail.com>
2021-11-09 19:37:26 +0100

    kill the @Ignore

--- src/test/java/EntityReferenceValuesTest.java
+++ src/test/java/EntityReferenceValuesTest.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
