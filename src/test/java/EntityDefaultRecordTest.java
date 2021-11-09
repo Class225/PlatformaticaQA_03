fd16e87adcb71c7014431c6c336933d8895128f9
Olena Kosenko <olena.kosenko.mail@gmail.com>
2021-11-09 10:36:47 -0800

    OlenaKosenko/Add_AT_003.01_Entity_Default_CreateNewRecord

--- src/test/java/EntityDefaultRecordTest.java
+++ src/test/java/EntityDefaultRecordTest.java
@@ -15,33 +15,66 @@ public class EntityDefaultRecordTest extends BaseTest {
     private static final By DEFAULT_MENU_LOCATOR = By.xpath("//p[contains(text(),'Default')]");
     private static final By ADD_NEW_FORM_BUTTON_LOCATOR = By.xpath("//i[contains(text(), 'create_new_folder')]");
     private static final By STRING_FIELD_LOCATOR = By.id("string");
-    private static final String STRING_INPUT = "Value of String Input";
     private static final By TEXT_FIELD_LOCATOR = By.id("text");
-    private static final String TEXT_INPUT = "Value of Text Input";
     private static final By INT_FIELD_LOCATOR = By.id("int");
-    private static final String INT_INPUT = "123";
     private static final By DECIMAL_FIELD_LOCATOR = By.id("decimal");
-    private static final String DECIMAL_INPUT = "987.14";
     private static final By DATE_FIELD_LOCATOR = By.id("date");
-    private static final String DATE_INPUT = "07/11/2021";
     private static final By DATETIME_FIELD_LOCATOR = By.id("datetime");
-    private static final String DATETIME_INPUT = "06/11/2021 17:22:46";
     private static final By ADD_BUTTON_LOCATOR = By.xpath("//button[contains(text(), '+')]");
     private static final By STRING_EMBED_FIELD_LOCATOR = By.id("t-11-r-1-string");
-    private static final String STRING_EMBED_INPUT = "String Input Value";
     private static final By TEXT_EMBED_FIELD_LOCATOR = By.id("t-11-r-1-text");
-    private static final String TEXT_EMBED_INPUT = "Text Input Value";
     private static final By INT_EMBED_FIELD_LOCATOR = By.id("t-11-r-1-int");
-    private static final String INT_EMBED_INPUT = "88";
     private static final By DECIMAL_EMBED_FIELD_LOCATOR = By.id("t-11-r-1-decimal");
-    private static final String DECIMAL_EMBED_INPUT = "41.17";
     private static final By DATE_EMBED_FIELD_LOCATOR = By.id("t-11-r-1-date");
-    private static final String DATE_EMBED_INPUT = "02/02/2021";
     private static final By DATETIME_EMBED_FIELD_LOCATOR = By.id("t-11-r-1-datetime");
-    private static final String DATETIME_EMBED_INPUT = "03/03/2021 18:05:10";
     private static final By SAVE_BUTTON_LOCATOR = By.id("pa-entity-form-save-btn");
 
-    public void clearSendKeys(By by, String input) {
+    private static final String STRING_INPUT = "Value of String Input";
+    private static final String TEXT_INPUT = "Value of Text Input";
+    private static final String INT_INPUT = "123";
+    private static final String DECIMAL_INPUT = "987.14";
+    private static final String DATE_INPUT = "07/11/2021";
+    private static final String DATETIME_INPUT = "06/11/2021 17:22:46";
+    private static final String STRING_EMBED_INPUT = "String Input Value";
+    private static final String TEXT_EMBED_INPUT = "Text Input Value";
+    private static final String INT_EMBED_INPUT = "88";
+    private static final String DECIMAL_EMBED_INPUT = "41.17";
+    private static final String DATE_EMBED_INPUT = "02/02/2021";
+    private static final String DATETIME_EMBED_INPUT = "03/03/2021 18:05:10";
+
+    private static final List<By> FORM_LOCATORS_LIST = Arrays.asList(
+            STRING_FIELD_LOCATOR,
+            TEXT_FIELD_LOCATOR,
+            INT_FIELD_LOCATOR,
+            DECIMAL_FIELD_LOCATOR,
+            DATE_FIELD_LOCATOR,
+            DATETIME_FIELD_LOCATOR);
+
+    private static final List<String> FORM_INPUT_LIST = Arrays.asList(
+            STRING_INPUT,
+            TEXT_INPUT,
+            INT_INPUT,
+            DECIMAL_INPUT,
+            DATE_INPUT,
+            DATETIME_INPUT);
+
+    private static final List<By> FORM_EMBED_LOCATORS_LIST = Arrays.asList(
+            STRING_EMBED_FIELD_LOCATOR,
+            TEXT_EMBED_FIELD_LOCATOR,
+            INT_EMBED_FIELD_LOCATOR,
+            DECIMAL_EMBED_FIELD_LOCATOR,
+            DATE_EMBED_FIELD_LOCATOR,
+            DATETIME_EMBED_FIELD_LOCATOR);
+
+    private static final List<String> FORM_EMBED_INPUT_LIST = Arrays.asList(
+            STRING_EMBED_INPUT,
+            TEXT_EMBED_INPUT,
+            INT_EMBED_INPUT,
+            DECIMAL_EMBED_INPUT,
+            DATE_EMBED_INPUT,
+            DATETIME_EMBED_INPUT);
+
+    public void setElementValue(By by, String input) {
         getDriver().findElement(by).clear();
         getDriver().findElement(by).sendKeys(input);
     }
@@ -56,52 +89,40 @@ public class EntityDefaultRecordTest extends BaseTest {
 
         getDriver().findElement(ADD_NEW_FORM_BUTTON_LOCATOR).click();
 
-        List<By> locatorsList = Arrays.asList(STRING_FIELD_LOCATOR, TEXT_FIELD_LOCATOR, INT_FIELD_LOCATOR,
-                DECIMAL_FIELD_LOCATOR, DATE_FIELD_LOCATOR, DATETIME_FIELD_LOCATOR);
-
-        List<String> inputList = Arrays.asList(STRING_INPUT, TEXT_INPUT, INT_INPUT, DECIMAL_INPUT,
-                DATE_INPUT, DATETIME_INPUT);
-
-        for (int i = 0; i < inputList.size(); i++) {
-            clearSendKeys(locatorsList.get(i), inputList.get(i));
+        for (int i = 0; i < FORM_INPUT_LIST.size(); i++) {
+            setElementValue(FORM_LOCATORS_LIST.get(i), FORM_INPUT_LIST.get(i));
         }
 
         TestUtils.scrollClick(getDriver(), ADD_BUTTON_LOCATOR);
 
-        List<By> locatorsEmbedList = Arrays.asList(STRING_EMBED_FIELD_LOCATOR, TEXT_EMBED_FIELD_LOCATOR,
-                INT_EMBED_FIELD_LOCATOR, DECIMAL_EMBED_FIELD_LOCATOR, DATE_EMBED_FIELD_LOCATOR, DATETIME_EMBED_FIELD_LOCATOR);
-
-        List<String> inputEmbedList = Arrays.asList(STRING_EMBED_INPUT, TEXT_EMBED_INPUT, INT_EMBED_INPUT,
-                DECIMAL_EMBED_INPUT, DATE_EMBED_INPUT, DATETIME_EMBED_INPUT);
-
-        for (int i = 0; i < inputEmbedList.size(); i++) {
-            if (i == 4 || i == 5) {
-                getDriver().findElement(locatorsEmbedList.get(i)).click();
+        for (int i = 0; i < FORM_EMBED_INPUT_LIST.size(); i++) {
+            if (FORM_EMBED_LOCATORS_LIST.get(i).toString().contains("date")) {
+                getDriver().findElement(FORM_EMBED_LOCATORS_LIST.get(i)).click();
             }
-            clearSendKeys(locatorsEmbedList.get(i), inputEmbedList.get(i));
+            setElementValue(FORM_EMBED_LOCATORS_LIST.get(i), FORM_EMBED_INPUT_LIST.get(i));
         }
 
         getDriver().findElement(SAVE_BUTTON_LOCATOR).click();
 
         List<WebElement> inputListLocators = getDriver().findElements(By.xpath("//td[@class= 'pa-list-table-th']"));
         Assert.assertTrue(inputListLocators.size() != 0);
-        for (int i = 0; i < inputList.size(); i++) {
-            Assert.assertEquals(inputListLocators.get(i).getText(), inputList.get(i));
+        for (int i = 0; i < FORM_INPUT_LIST.size(); i++) {
+            Assert.assertEquals(inputListLocators.get(i).getText(), FORM_INPUT_LIST.get(i));
         }
 
         getDriver().findElement(By.xpath("//tbody")).click();
 
         List<WebElement> inputListDetails = getDriver().findElements(By.xpath(" //span[@class = 'pa-view-field']"));
         Assert.assertTrue(inputListDetails.size() != 0);
-        for (int i = 0; i < inputList.size(); i++) {
-            Assert.assertEquals(inputListDetails.get(i).getText(), inputList.get(i));
+        for (int i = 0; i < FORM_INPUT_LIST.size(); i++) {
+            Assert.assertEquals(inputListDetails.get(i).getText(), FORM_INPUT_LIST.get(i));
         }
 
         List<WebElement> inputEmbedListLocators = getDriver().findElements(By.xpath(" //td"));
         inputEmbedListLocators.remove(0);
         Assert.assertTrue(inputEmbedListLocators.size() != 0);
-        for (int i = 0; i < inputEmbedList.size(); i++) {
-            Assert.assertEquals(inputEmbedListLocators.get(i).getText(), inputEmbedList.get(i));
+        for (int i = 0; i < FORM_EMBED_INPUT_LIST.size(); i++) {
+            Assert.assertEquals(inputEmbedListLocators.get(i).getText(), FORM_EMBED_INPUT_LIST.get(i));
         }
     }
 }
