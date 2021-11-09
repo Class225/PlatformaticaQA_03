c42c1aa8aa6e732c30478fb568ee092352db3b42
Olena Kosenko <olena.kosenko.mail@gmail.com>
2021-11-09 12:35:58 -0800

    OlenaKosenko/Add_AT_003.01_Entity_Default_CreateNewRecord_Add_logs_to identify_CI_failure

--- src/test/java/EntityDefaultRecordTest.java
+++ src/test/java/EntityDefaultRecordTest.java
@@ -118,9 +118,11 @@ public class EntityDefaultRecordTest extends BaseTest {
         compareDataDisplayedWithEntered(webElementPreviewFormList, FORM_INPUT_LIST);
         System.out.println("Page is displayed: " + getDriver().findElement(By.xpath("//span[contains(text(),'Showing 1 to 1 of 1 rows')]")).getText());
 
-        getDriver().findElement(By.xpath("//tbody")).click();
+       // getDriver().findElement(By.xpath("//tbody")).click();
 
-        System.out.println("Page is displayed: " + getDriver().findElement(By.xpath("//span[contains(text(),'Showing 1 to 1 of 1 rows')]")).getText());
+        getDriver().findElement(By.xpath("//td[@class= 'pa-list-table-th']")).click();
+
+  //      System.out.println("Page is displayed: " + getDriver().findElement(By.xpath("//span[contains(text(),'Showing 1 to 1 of 1 rows')]")).getText());
         System.out.println("Page is displayed: " + getDriver().findElement(By.xpath("//h4")).getText());
         System.out.println("Page is displayed: " + getDriver().findElement(By.xpath("//h4[contains(text(),'EmbedD')]")).getText());
 
