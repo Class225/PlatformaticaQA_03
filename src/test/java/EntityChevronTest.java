47f80a34989431cda720734fa6100d2a72f79015
Ilia Ponomarev <ponomid@ya.ru>
2021-11-10 11:39:54 -0800

    AT_014.01.05 | Entity > Chevron> Input (New record with correct data) (#341)
    * Added new test testCreateRecordPending()
    
    * Added new test testCreateRecordFulfillment()
    
    * Added new test testCreateRecordPending()
    
    * Added new test testCreateRecordPending()
    
    * Added new test testCreateRecordFulfillment()
    
    * Added new test testCreateRecordSent()
    
    * Added new test testCreateRecordWithValidData()
    
    * Refactoring
    
    Co-authored-by: Ilia <ponomidya.ru>

--- src/test/java/EntityChevronTest.java
+++ src/test/java/EntityChevronTest.java
@@ -6,20 +6,22 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.TestUtils;
-
 import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class EntityChevronTest extends BaseTest {
 
     private static final By ENTITIES_TAB_IN_MENU = By.xpath("//a[@href='#menu-list-parent']");
-    private static final By ENTITYCHEVRON = By.xpath("//p[text()=' Chevron ']");
-    private static final By CREATENEWFOLDER = By.xpath("//i[text()='create_new_folder']");
-    private static final By STRINGSTATUSMENU = By.xpath("//div[@class='filter-option-inner-inner'][text()='Pending']");
-    private static final By SAVEBUTTON = By.id("pa-entity-form-save-btn");
+    private static final By ENTITY_CHEVRON = By.xpath("//p[text()=' Chevron ']");
+    private static final By CREATE_NEW_FOLDER = By.xpath("//i[text()='create_new_folder']");
+    private static final By STRING_STATUS_MENU = By.xpath("//div[@class='filter-option-inner-inner'][text()='Pending']");
+    private static final By SAVE_BUTTON = By.id("pa-entity-form-save-btn");
     private static final By SAVE_DRAFT_BUTTON = By.id("pa-entity-form-draft-btn");
     private static final By STRING_DROPDOWN = By.id("string");
+    private static final By STRING_STATUS_PENDING = By.xpath("//span[text()='Pending']");
 
-    public void moveMouseToEntityTabInMenu(){
+    public void moveMouseToEntityTabInMenu() {
         Actions moveMouse = new Actions(getDriver());
         moveMouse.moveToElement(getDriver().findElement(ENTITIES_TAB_IN_MENU)).perform();
     }
@@ -29,22 +31,21 @@ public class EntityChevronTest extends BaseTest {
         select.selectByVisibleText(visibleText);
     }
 
-
     @Test
-    public void testCreateRecord(){
-        getDriver().findElement(ENTITYCHEVRON).click();
-        getDriver().findElement(CREATENEWFOLDER).click();
-        getDriver().findElement(SAVEBUTTON).click();
+    public void testCreateRecord() {
+        getDriver().findElement(ENTITY_CHEVRON).click();
+        getDriver().findElement(CREATE_NEW_FOLDER).click();
+        getDriver().findElement(SAVE_BUTTON).click();
 
         Assert.assertTrue(getDriver().findElement(By.xpath("//tr[@data-index='0']")).isDisplayed());
     }
 
     @Test
-    public void testSaveDraftPending(){
+    public void testSaveDraftPending() {
         String testingData = "Pending";
         moveMouseToEntityTabInMenu();
-        TestUtils.scrollClick(getDriver(), getDriver().findElement(ENTITYCHEVRON));
-        getDriver().findElement(CREATENEWFOLDER).click();
+        TestUtils.scrollClick(getDriver(), getDriver().findElement(ENTITY_CHEVRON));
+        getDriver().findElement(CREATE_NEW_FOLDER).click();
         selectByVisibleText(testingData);
         getDriver().findElement(SAVE_DRAFT_BUTTON).click();
 
@@ -53,11 +54,11 @@ public class EntityChevronTest extends BaseTest {
     }
 
     @Test
-    public void testSaveDraftFulfillment(){
+    public void testSaveDraftFulfillment() {
         String testingData = "Fulfillment";
         moveMouseToEntityTabInMenu();
-        TestUtils.scrollClick(getDriver(), getDriver().findElement(ENTITYCHEVRON));
-        getDriver().findElement(CREATENEWFOLDER).click();
+        TestUtils.scrollClick(getDriver(), getDriver().findElement(ENTITY_CHEVRON));
+        getDriver().findElement(CREATE_NEW_FOLDER).click();
         selectByVisibleText(testingData);
         getDriver().findElement(SAVE_DRAFT_BUTTON).click();
 
@@ -66,21 +67,64 @@ public class EntityChevronTest extends BaseTest {
     }
 
     @Test
-    public void testCreateRecordPending(){
-        getDriver().findElement(ENTITYCHEVRON).click();
-        getDriver().findElement(CREATENEWFOLDER).click();
-        getDriver().findElement(STRINGSTATUSMENU).click();
+    public void testCreateRecordPending() {
+        getDriver().findElement(ENTITY_CHEVRON).click();
+        getDriver().findElement(CREATE_NEW_FOLDER).click();
+        getDriver().findElement(STRING_STATUS_MENU).click();
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         getDriver().findElement(By.xpath("//span[text()='Pending']")).click();
-        getDriver().findElement(SAVEBUTTON).click();
+        getDriver().findElement(SAVE_BUTTON).click();
 
         List<WebElement> recordValues = getDriver().findElements(By.xpath("//td[@class='pa-list-table-th']"));
 
         Assert.assertEquals(recordValues.get(0).getText(), "Pending");
+    }
+
+    @Test
+    public void testCreateRecordWithValidData() throws InterruptedException {
+        final String InputText = "Test";
+        final String InputInt = "1";
+        final String InputDecimal = "2.00";
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+
+        SimpleDateFormat formatterTime = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date time = new Date();
+
+        getDriver().findElement(ENTITY_CHEVRON).click();
+        getDriver().findElement(CREATE_NEW_FOLDER).click();
+        getDriver().findElement(STRING_STATUS_MENU).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        getDriver().findElement(STRING_STATUS_PENDING).click();
+        getDriver().findElement(By.id("text")).sendKeys(InputText);
+        Thread.sleep(1000);
+        getDriver().findElement(By.id("int")).sendKeys(InputInt);
+        getDriver().findElement(By.id("decimal")).sendKeys(InputDecimal);
+        getDriver().findElement(By.id("date")).click();
+        getDriver().findElement(By.id("datetime")).click();
+        getDriver().findElement(By.xpath("//div[text() ='apptester1@tester.test']")).click();
+
+        getDriver().findElement(SAVE_BUTTON).click();
+
+        List<WebElement> recordValues = getDriver().findElements(By.xpath("//td[@class='pa-list-table-th']"));
+
+        Assert.assertEquals(recordValues.get(0).getText(), "Pending");
+        Assert.assertEquals(recordValues.get(1).getText(), InputText);
+        Assert.assertEquals(recordValues.get(2).getText(), InputInt);
+        Assert.assertEquals(recordValues.get(3).getText(), InputDecimal);
+        Assert.assertEquals(recordValues.get(4).getText(), formatter.format(date));
+        Assert.assertEquals(recordValues.get(5).getText().substring(0, 16), formatterTime.format(time));
+        Assert.assertTrue(recordValues.get(6).getText().isEmpty());
+        Assert.assertEquals(recordValues.get(7).getText(), "apptester1@tester.test");
     }
 
 }
