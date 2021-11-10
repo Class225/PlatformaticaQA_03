50e68e7e4872ccdc0b9ccc771d40330f254430cb
Vladimir <umistester@gmail.com>
2021-11-10 23:38:45 +0300

    Nov10

--- src/test/java/old/VladimirMTest.java
+++ src/test/java/old/VladimirMTest.java
@@ -8,21 +8,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+// import java.util.concurrent.TimeUnit;
 
 public class VladimirMTest {
 
     @Test
-    public void testSelenium() {
+    public void testSelenium() throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
-//        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+// driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+// driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 
         driver.get("https://av.by");
 
@@ -40,12 +40,11 @@ public class VladimirMTest {
                 "/div[4]/form[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/ul[1]/li[27]/button[1]"));
         modelX7.click();
 
-        WebElement show = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[1]/div" +
-                "[4]/form[1]/div[1]/div[3]/div[1]/div[3]/div[2]/div[2]/button[1]"));
+        WebElement show = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[1]" +
+                "/div[4]/form[1]/div[1]/div[3]/div[1]/div[3]/div[2]/div[2]/button[1]/span[1]"));
         show.click();
 
-        WebElement car = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[1]" +
-                "/div[6]/div[3]/div[1]/div[3]/div[1]/div[1]/div[1]/div[2]/h3[1]/a[1]/span[1]"));
+        Thread.sleep(100);
 
         String expectedUrl = "https://cars.av.by/bmw/x7";
         String actualUrl = driver.getCurrentUrl();
