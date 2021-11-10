77bbfd270df99f92eae7c2405f333f508d09b154
Ilia Ponomarev <ponomid@ya.ru>
2021-11-10 10:54:59 -0800

    AT_014.01.02 | Entity > Chevron> Input (New record) Pending (#338)
    * Added new test testCreateRecordPending()
    
    * Added new test testCreateRecordFulfillment()
    
    * Added new test testCreateRecordPending()
    
    * Added new test testCreateRecordPending()
    
    Co-authored-by: Ilia <ponomidya.ru>

--- src/test/java/EntityChevronTest.java
+++ src/test/java/EntityChevronTest.java
@@ -7,11 +7,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.TestUtils;
 
+import java.util.List;
+
 public class EntityChevronTest extends BaseTest {
 
     private static final By ENTITIES_TAB_IN_MENU = By.xpath("//a[@href='#menu-list-parent']");
     private static final By ENTITYCHEVRON = By.xpath("//p[text()=' Chevron ']");
     private static final By CREATENEWFOLDER = By.xpath("//i[text()='create_new_folder']");
+    private static final By STRINGSTATUSMENU = By.xpath("//div[@class='filter-option-inner-inner'][text()='Pending']");
     private static final By SAVEBUTTON = By.id("pa-entity-form-save-btn");
     private static final By SAVE_DRAFT_BUTTON = By.id("pa-entity-form-draft-btn");
     private static final By STRING_DROPDOWN = By.id("string");
@@ -60,6 +63,24 @@ public class EntityChevronTest extends BaseTest {
 
         Assert.assertTrue(getDriver().findElement(By.xpath("//tr[@data-index='0']//i[@class='fa fa-pencil']")).isDisplayed());
         Assert.assertEquals(getDriver().findElement(By.xpath("//table//a[contains(text(), 'Fulfillment')]")).getText(), testingData);
+    }
+
+    @Test
+    public void testCreateRecordPending(){
+        getDriver().findElement(ENTITYCHEVRON).click();
+        getDriver().findElement(CREATENEWFOLDER).click();
+        getDriver().findElement(STRINGSTATUSMENU).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        getDriver().findElement(By.xpath("//span[text()='Pending']")).click();
+        getDriver().findElement(SAVEBUTTON).click();
+
+        List<WebElement> recordValues = getDriver().findElements(By.xpath("//td[@class='pa-list-table-th']"));
+
+        Assert.assertEquals(recordValues.get(0).getText(), "Pending");
     }
 
 }
